aabfd35ad34504c46bd5d5de50dc87a75eb326d6
deepakchauhan33 <deep651965@gmail.com>
2025-12-24 23:57:42 +0530

    EP new question

--- DOM/script.js
+++ DOM/script.js
@@ -1,16 +1,41 @@
-const GrandParent = document.querySelector("#grandParent");
-const Parent = document.querySelector("#parent");
-const Child = document.querySelector("#child");
+// const GrandParent = document.querySelector("#grandParent");
+// const Parent = document.querySelector("#parent");
+// const Child = document.querySelector("#child");
 
 
-GrandParent.addEventListener('click', function(){
-    console.log("Grand Parent");
-},false)
+// GrandParent.addEventListener('click', function(){
+//     console.log("Grand Parent");
+// },false)
 
-Parent.addEventListener('click', function(){
-    console.log("Parent");
-}, false)
+// Parent.addEventListener('click', function(){
+//     console.log("Parent");
+// }, false)
 
-Child.addEventListener('click', function(){
-    console.log("Child");
-}, false)
+// Child.addEventListener('click', function(){
+//     console.log("Child");
+// }, false)
+
+
+const tableBody = document.querySelector("tbody");
+
+
+function calcTotal() {
+
+    let total = 0;
+    const allPriceElements = document.querySelectorAll('[data-ns-test="price"]');
+
+    allPriceElements.forEach(el => {
+        total += Number(el.textContent);
+
+    });
+
+
+    const totalRow = document.createElement("tr");
+totalRow.innerHTML = `<td>Total</td> <td>${total}</td>`
+
+tableBody.appendChild(totalRow);
+
+
+    
+
+}
