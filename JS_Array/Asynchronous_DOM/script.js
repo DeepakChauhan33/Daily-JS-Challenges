4eb71a8daf313d7c01674fbeed01042082e077f2
deepakchauhan33 <deep651965@gmail.com>
2025-12-31 22:41:14 +0530

    New Question

--- JS_Array/Asynchronous_DOM/script.js
+++ JS_Array/Asynchronous_DOM/script.js
@@ -1,9 +1,45 @@
 
 
 
-function sayHello(naam){
-    console.log("Hello "+ naam);
+// function sayHello(naam){
+//     console.log("Hello "+ naam);
+// }
+
+// setTimeout(sayHello,3000, "Dia");
+
+
+
+function calculateBMI(weight, height) {
+   
+      if (weight <= 0 || height <= 0) {
+        return "Weight(in kg) and height(in meters) must be positive values";
+    }
+
+    const bmi = weight / (height * height);
+
+
+    if (bmi < 18.5) {
+        return "Underweight";
+    } else if (bmi >= 18.5 && bmi < 24.9) {  // Here it uses or operator instead of and operator
+        return "Normal Weight"
+    } else if (bmi >= 25 && bmi < 29.9) { // here also it uses or operator instead of and operator
+        return "Over Weight"
+    } else {
+        return "Obese"
+    }
+//     const bmi = weight / (height * height);
+
+//     if (bmi < 18.5) {
+//         return "Underweight";
+//     } else if (bmi >= 18.5 || bmi < 24.9) {
+//         return "Normal weight";
+//     } else if (bmi >= 25 || bmi < 29.9) {
+//         return "Overweight";
+//     } else {
+//         return "Obese";
+//     }
 }
 
-setTimeout(sayHello,3000, "Dia");
+// Do not modify the below lines
 
+console.log(calculateBMI(60,1.95))
