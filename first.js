85913e80785fa9065b80146af92dee68aed94cc5
deepakchauhan33 <deep651965@gmail.com>
2026-04-05 19:15:24 +0530

    New Question

--- first.js
+++ first.js
@@ -39,50 +39,50 @@
 
 
 
-var buyChoco = function(prices, money) {
-    
+var buyChoco = function (prices, money) {
+
     const sortPrices = prices.sort();
-    const val = sortPrices[0]+sortPrices[1];
+    const val = sortPrices[0] + sortPrices[1];
     console.log(val)
 
-    if(money>=val){
-        return money-val;
-    }else{
+    if (money >= val) {
+        return money - val;
+    } else {
         return money
     }
-    
+
 };
 
 
-console.log(buyChoco([1,2,2],3))
+console.log(buyChoco([1, 2, 2], 3))
 
 
 
-var searchRange = function(nums, target){
-  
-   const result = [];
+var searchRange = function (nums, target) {
 
-   for(let i=0; i<-nums.length;i++){
+    const result = [];
 
-    if(nums[i] === target){
-        result.push(i);
+    for (let i = 0; i < -nums.length; i++) {
+
+        if (nums[i] === target) {
+            result.push(i);
+        }
+
     }
-    
-   }
 
 
 };
 
 
 
-const plusOne = function(digits){
-    
+const plusOne = function (digits) {
+
     // const n = digits.length;
 
     // const newArr = [];
-    
+
     // for(let i=0; i<n; i++){
-       
+
     //     if(i===n-1){
     //         let num = digits[i]+1;
     //         newArr.push(num);
@@ -93,12 +93,12 @@ const plusOne = function(digits){
     //         console.log(1)
     //     }
     // }
-    
+
     // return newArr
 
 
-    return (Number(digits.join(''))+1).toString().split('').map(Number);
-    
+    return (Number(digits.join('')) + 1).toString().split('').map(Number);
+
 };
 
 
@@ -106,20 +106,20 @@ const plusOne = function(digits){
 
 
 
-function fibo(num){
+function fibo(num) {
 
-    if(num <= 0) {
+    if (num <= 0) {
         return 0;
     }
-    if(num === 1) return 1;
+    if (num === 1) return 1;
 
     let firstNum = 0;
     let secondNum = 1;
-    let i=0;
-    while(i<num){
+    let i = 0;
+    while (i < num) {
         console.log(firstNum);
         console.log(secondNum);
-        let third = firstNum+secondNum;
+        let third = firstNum + secondNum;
         console.log(third);
     }
 
@@ -130,3 +130,44 @@ function fibo(num){
 
 
 // 0 1 1 2 3 5 8 13 
+
+
+
+function arraySign(arr) {
+
+
+    let positive = 0;
+    let negative = 0;
+    let zero = false;
+    arr.forEach(num => {
+        if (num > 0) positive++;
+        if (num < 0) negative++;
+        if (num === 0) zero = true;
+
+
+    });
+
+    if (zero) {
+        return 0;
+    } else if (negative > positive) {
+        return -1;
+    } else {
+        return 1;
+    }
+    // console.log(`positive : ${positive}  negative : ${negative}  zero : ${zero}`)
+}
+
+
+let sign = arraySign([-1, -2, -3])
+console.log(sign);
+
+
+// Given a set of rectangles, each defined by its width and height, your task is to determine how many rectangles can form the largest possible square. A square can be formed from a rectangle if both its width and height are greater than or equal to the side length of the square.
+
+// You are given an array rectangles where rectangles[i] = [li, wi] represents the ith rectangle of length li and width wi.
+
+// For example, if you have a rectangle [4,6], you can cut it to get a square with a side length of at most 4.
+
+// Let maxLen be the side length of the largest square you can obtain from any of the given rectangles.
+
+// Return the number of rectangles that can make a square with a side length of maxLen.
